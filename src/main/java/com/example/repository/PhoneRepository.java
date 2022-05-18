d46c860546c3366764c63f2ef65b6554e4f2c749
Yousef Khaled <49095488+YousefKhaled0@users.noreply.github.com>
2022-05-18 03:14:24 +0200

    Delete src directory

--- src/main/java/com/example/repository/PhoneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Phone;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PhoneRepository extends MongoRepository<Phone, String> {
-}
